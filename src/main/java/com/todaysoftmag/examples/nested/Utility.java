3570b779e4316c7992a76edef857811f75d119cd
Tavi Bolog <tavi.bolog@gmail.com>
2012-04-23 00:32:34 +0200

    example for nested statements - more comments

--- src/main/java/com/todaysoftmag/examples/nested/Utility.java
+++ src/main/java/com/todaysoftmag/examples/nested/Utility.java
@@ -11,22 +11,35 @@ import com.todaysoftmag.examples.objectmethods.Car;
  * 
  */
 public class Utility {
+    /**
+     * print the elements of a cars list. There is no closing statement.
+     * 
+     * @param listOfCars
+     */
     public static void printWithNested(List<Car> listOfCars) {
         if (listOfCars != null) {
             for (Car car : listOfCars) {
                 if (car != null) {
                     System.out.println("car: " + car);
+                } else {
+                    System.out.println("car is null");
                 }
             }
         }
     }
 
+    /**
+     * print the elements of a cars list. The closing statements are used wherever possible: return or continue
+     * 
+     * @param listOfCars
+     */
     public static void printWithoutNested(List<Car> listOfCars) {
         if (listOfCars == null) {
             return;
         }
         for (Car car : listOfCars) {
             if (car == null) {
+                System.out.println("car is null");
                 continue;
             }
             System.out.println("car: " + car);
